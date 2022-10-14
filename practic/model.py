28defd58901df2dfb3828a5115c80d1299b5d9de
tkdany <tkdany@yandex.ru>
2022-10-14 09:19:12 +0300

    idk

--- practic/model.py
+++ practic/model.py
@@ -1,3 +1,8 @@
+# Нужен файл, куда записываются данные!
+
+
+import matplotlib.pyplot as plt
+
 L = 1000  # постоянные
 K = 0.5
 
@@ -6,6 +11,11 @@ W = 0
 day = 0
 statistic = [1]
 queue_index = 0
+
+N_values = [1]
+W_values = [0]
+
+file = open("answer.txt", "w")
 while N > 0:
     Z = K * ((L - N - W) / L) * N
     statistic.append(Z)
@@ -16,7 +26,26 @@ while N > 0:
         queue_index += 1
     W = W + V
     N = N + Z - V
+    N_values.append(N)
+    W_values.append(W)
     day += 1
-    print(N, Z, W)
+    file.write(f"День {day}. Всего заболевших - {N}. Всего выздоровело - {W}. \n")
+    if day == 100:
+        break
 
-print(day)
+days = [i for i in range(0, 101)]
+
+fig, ax = plt.subplots()
+
+ax.plot(days, N_values, label="Количество заболевших")
+ax.plot(days, W_values, label="Количество переболевших")
+
+ax.legend()
+
+plt.title("Модель эпидемии гриппа")
+plt.xlabel("День")
+plt.ylabel("Кол-во человек")
+plt.grid()
+plt.show()
+
+file.close()
