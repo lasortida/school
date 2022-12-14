b466b00d3ae5c5f933d07c4c6a150fb0f1bc5fce
tkdany <tkdany@yandex.ru>
2022-12-14 11:15:26 +0300

    idk

--- main.py
+++ main.py
@@ -1,43 +1,37 @@
-# def f(n, m):
-#     return n % m == 0
-#
-#
-# for A in range(-1000, 100000):
-#     ok = True
-#     for x in range(1, 10000):
-#         if not((f(72, x) <= (not (f(120, x)))) or (A - x > 100)):
-#             ok = False
-#             break
-#     if ok:
-#         print(A)
-#         break
-
-
-# def f(n):
-#     if n == 0:
-#         return 0
-#     else:
-#         return f(n - 1) + n
-#
-#
-# print((1134567004 - 237567892 + 1) // 3)
-
-
-file = open("17.txt")
-data = [int(x) for x in file.readlines()]
-mini = 10000
+row = input()
+data = row.split(" ")
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+stack = []
+let = set()
+map = {}
 for el in data:
-    if str(el)[-1] == "7" and el < mini:
-        mini = el
-count = 0
-maxi = 0
-for i in range(len(data) - 1):
-    first = data[i]
-    second = data[i + 1]
-    row = str(first)[-1] + str(second)[-1]
-    if row.count("7") == 1 and (first ** 2 + second ** 2) < mini ** 2:
-        count += 1
-        if first ** 2 + second ** 2 > maxi:
-            maxi = first ** 2 + second ** 2
-print(count)
-print(maxi)
+    if el in alphabet:
+        let.add(el)
+count = len(let)
+for i in range(count):
+    string = input()
+    a = string.split("=")
+    map[a[0]] = a[1]
+print(map)
+for el in row:
+    if el in alphabet:
+        number = map[el]
+        stack.append(number)
+    if type(el) == int:
+        let.add(int(el))
+    else:
+        if el in "+-*/":
+            if len(stack) > 1:
+                c = stack.pop()
+                d = stack.pop()
+                if el == "+":
+                    stack.append(c + d)
+                elif el == "-":
+                    stack.append(d - c)
+                elif el == "*":
+                    stack.append(c * d)
+                else:
+                    stack.append(d / c)
+        else:
+
+
